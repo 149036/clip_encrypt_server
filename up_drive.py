1a8b413cadb863cb21e6158400f45503ab874cba
149036 <96453230+149036@users.noreply.github.com>
2024-02-23 09:03:32 +0900

    Update up_drive.py

--- up_drive.py
+++ up_drive.py
@@ -1,26 +1,26 @@
-import requests
-import json
+# import requests
+# import json
 
 
-def upload_to_google_drive(
-    video_path="",
-    target_name="",
-    access_token="",
-    drive_folder_id="",
-):
+# def upload_to_google_drive(
+#     video_path="",
+#     target_name="",
+#     access_token="",
+#     drive_folder_id="",
+# ):
 
-    headers = {"Authorization": f"Bearer {access_token}"}
-    params = {
-        "name": target_name,
-        "parents": [drive_folder_id],
-    }
-    files = {
-        "data": ("metadata", json.dumps(params), "application/json; charset=UTF-8"),
-        "file": open(video_path, "rb"),
-    }
-    response = requests.post(
-        url="https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
-        headers=headers,
-        files=files,
-    )
-    print(response.json())
+#     headers = {"Authorization": f"Bearer {access_token}"}
+#     params = {
+#         "name": target_name,
+#         "parents": [drive_folder_id],
+#     }
+#     files = {
+#         "data": ("metadata", json.dumps(params), "application/json; charset=UTF-8"),
+#         "file": open(video_path, "rb"),
+#     }
+#     response = requests.post(
+#         url="https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+#         headers=headers,
+#         files=files,
+#     )
+#     print(response.json())
