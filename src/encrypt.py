183be5a7fdcb80ccd2615405129031e9a31309d2
149036 <96453230+149036@users.noreply.github.com>
2024-02-28 18:12:05 +0900

    Create encrypt.py

---
+++ src/encrypt.py
@@ -0,0 +1,43 @@
+import subprocess
+
+
+def aes_256_cbc(
+    normal_files,
+    dl_path,
+    user_path,
+):
+    print("encrypt : start")
+    for target in normal_files:
+        target_path = dl_path + "/" + target
+        output_path = user_path + "/encrypted/encrypted-" + target
+
+        subprocess.run(
+            [
+                "openssl",
+                "enc",
+                "-e",
+                "-aes-256-cbc",
+                "-a",
+                "-salt",
+                "-k",
+                "password",
+                "-pbkdf2",
+                "-in",
+                target_path,
+                "-out",
+                output_path,
+            ],
+            check=True,
+        )
+
+        # キャシュの開放
+        cmd = "echo 1 | sudo tee /proc/sys/vm/drop_caches"
+        subprocess.run(
+            cmd,
+            shell=True,
+            capture_output=True,
+            check=True,
+        )
+
+        print(f"encrypted : {target}")
+    print("encrypt : end")
