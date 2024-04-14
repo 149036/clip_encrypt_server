5dcc0928e290bd06b5979c5c453a7d444f7c1748
149036 <sqrt2c@gmail.com>
2024-04-15 03:48:09 +0900

    change pass

--- src/encrypt.py
+++ src/encrypt.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from src import clear
 
@@ -8,9 +9,12 @@ def aes_256_cbc(
     user_path,
 ):
     print("encrypt : start")
+    pass_list = {}
     for target in normal_files:
         target_path = dl_path + "/" + target
         output_path = user_path + "/encrypted/encrypted-" + target
+
+        password = os.urandom(32).hex()
 
         subprocess.run(
             [
@@ -21,7 +25,7 @@ def aes_256_cbc(
                 "-a",
                 "-salt",
                 "-k",
-                "password",
+                password,
                 "-pbkdf2",
                 "-in",
                 target_path,
@@ -30,9 +34,12 @@ def aes_256_cbc(
             ],
             check=True,
         )
+        pass_list["encrypted-" + target] = password
 
         # キャシュの開放
         clear.cache()
 
         print(f"encrypted : {target}")
+    print(pass_list)
     print("encrypt : end")
+    return pass_list
