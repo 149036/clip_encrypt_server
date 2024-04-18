5ede5a455ec1fad24b26b5ee45adf4a6950bbb68
149036 <sqrt2c@gmail.com>
2024-04-18 12:04:28 +0900

    openssl用のconfigを追加

--- src/encrypt.py
+++ src/encrypt.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import configparser
 from src import clear
 
 
@@ -9,32 +10,66 @@ def aes_256_cbc(
     user_path,
 ):
     print("encrypt : start")
+    config_ini = configparser.ConfigParser()
+    config_ini.read("../config.ini", encoding="utf-8")
+    openssl = config_ini.get("DEFAULT", "openssl")
     pass_list = {}
+
     for target in normal_files:
         target_path = dl_path + "/" + target
         output_path = user_path + "/encrypted/encrypted-" + target
 
         password = os.urandom(32).hex()
 
-        subprocess.run(
-            [
-                "openssl",
-                "enc",
-                "-e",
-                "-aes-256-cbc",
-                "-a",
-                "-salt",
-                "-k",
-                password,
-                "-pbkdf2",
-                "-in",
-                target_path,
-                "-out",
-                output_path,
-            ],
-            check=True,
-        )
-        pass_list["encrypted-" + target] = password
+        openssl = "libressl"
+
+        if "openssl" == openssl:
+            subprocess.run(
+                [
+                    "openssl",
+                    "enc",
+                    "-e",
+                    "-aes-256-cbc",
+                    "-a",
+                    "-salt",
+                    "-k",
+                    password,
+                    "-pbkdf2",
+                    "-in",
+                    target_path,
+                    "-out",
+                    output_path,
+                ],
+                check=True,
+            )
+            pass_list["encrypted-" + target] = password
+
+        elif "libressl" == openssl:
+            key_bytes = os.urandom(32)
+            iv_bytes = os.urandom(16)
+            key = key_bytes.hex()
+            iv = iv_bytes.hex()
+            subprocess.run(
+                [
+                    "openssl",
+                    "enc",
+                    "-e",
+                    "-aes-256-cbc",
+                    "-base64",
+                    "-nosalt",
+                    "-p",
+                    "-K",
+                    key,
+                    "-iv",
+                    iv,
+                    "-in",
+                    target_path,
+                    "-out",
+                    output_path,
+                ],
+                check=True,
+            )
+            pass_list["encrypted-" + target] = {"key": key, "iv": iv}
 
         # キャシュの開放
         clear.cache()
